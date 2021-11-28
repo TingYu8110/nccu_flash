7ee0f8a89479ea2f8f83cb23caccb2e19d74a319
yu811026@gmail.com <yu811026@gmail.com>
2021-11-29 00:21:50 +0800

    all done

--- html/eye/assets/js/main.js
+++ html/eye/assets/js/main.js
@@ -1,3 +1,5 @@
+score = 0;
+count = 1;
 $(function(){
     var sound_playing = false;
     var sound = new Howl({
@@ -19,181 +21,98 @@ $(function(){
 
     calVh();
     window.addEventListener("resize", calVh);
-
-    // start
-    var start = $(".wrap__start");
-    var items = $(".wrap__title, .wrap__bg-deco, .wrap__bg-rotate-img, start-button");
-    var src = start.attr("src");
-    start.attr("src", "");
-    start.attr("src", src);
-    setTimeout(function(){
-        start.hide();
-        items.show();
-    }, 6000);
 })
     .on("click", ".start-button", function(){
-        var scene_2 = $(".wrap__scene-2");
-        var white = $(".wrap__white");
-        var src = scene_2.data("end");
-        var hide_items = $(".title-button, .wrap__bg-deco, .wrap__bg-rotate");
-        hide_items.hide();
+        var wrap_outer = $(".wrap-outer");
+        var title_1 = $(".wrap__title-1");
+        var title_2 = $(".wrap__title-2");
+        var title_3 = $(".wrap__title-3");
+        var title_man = $(".wrap__title-man");
+        var start_button = $(".start-button");
+        var hide_items = $(".wrap__title-1, .wrap__title-2, .wrap__title-3, .wrap__title-man, .start-button");
 
-        scene_2.attr("src", "");
-        scene_2.attr("src", src);
-        scene_2.show();
+        wrap_outer.addClass("wrap-outer--fading")
+        anime.timeline({
+            easing: "easeOutQuad",
+            complete: function(){
+                hide_items.hide();
 
-        setTimeout(function(){
-            white.animate({
-                opacity: 1
-            },250, function (){
                 var wrap_outer = $(".wrap-outer");
                 wrap_outer.addClass("wrap-outer--begin");
-            });
-        }, 750);
+
+                anime({
+                    targets: ".wrap__man",
+                    opacity: [0, 1],
+                    duration: 500,
+                    easing: "easeOutQuad"
+                });
+            }
+        })
+        .add({
+            targets: title_1[0],
+            opacity: 0,
+            translateX: 50,
+            duration: 500,
+        })
+        .add({
+            targets: title_2[0],
+            opacity: 0,
+            translateX: 50,
+            duration: 500,
+        }, "-=500")
+        .add({
+            targets: title_3[0],
+            rotate: "-45deg",
+            opacity: 0,
+            duration: 500,
+        }, "-=500")
+        .add({
+            targets: title_man[0],
+            translateY: -50,
+            opacity: 0,
+            duration: 500,
+        }, "-=500")
+        .add({
+            targets: start_button[0],
+            translateY: 10,
+            opacity: 0,
+            duration: 500,
+        }, "-=500");
 
     })
     .on("click", ".yes-button, .no-button", function(){
         var frame = $(".wrap-2").attr("data-frame");
         var which_btn = $(this).hasClass("yes-button");
-        var transition = $(".wrap__transition");
-        var transition_img = transition.find("img");
-        var src = transition_img.attr("src");
-        transition.css("transition", "");
-        transition.addClass("show");
-        transition_img.attr("src", "");
-        transition_img.attr("src", src);
-        setTimeout(function(){
-            transition.removeClass("show").animate({
-                opacity: 0
-            }, 250, function(){
-                transition.css("opacity", "");
+        changeFrame(which_btn, frame);
+        if (count < 19) {
+            anime({
+                targets: ".wrap__man",
+                opacity: [0, 1],
+                duration: 500,
+                easing: "easeOutQuad"
             });
-            changeFrame(which_btn, frame);
-        }, 500);
+        }
     })
     .on("click", ".replay-button", function(){
         window.location.reload();
-    })
-    .on("click", ".title-button", function(){
-        var scene_2 = $(".wrap__scene-2");
-        var start_btn = $(".start-button");
-        var src = scene_2.attr("src");
-        scene_2.attr("src", "");
-        scene_2.attr("src", src);
-        scene_2.show();
-        setTimeout(function(){
-            start_btn.show();
-        }, 500);
     });
 
 function changeFrame(if_right, frame) {
     var wrap = $(".wrap-2");
-    switch (frame) {
-        case "1":
-            if (if_right) {
-                wrap.attr("data-frame", "2");
-            } else {
-                wrap.attr("data-frame", "7");
-            }
-            break;
-        case "2":
-            if (if_right) {
-                wrap.attr("data-frame", "3");
-            } else {
-                wrap.attr("data-frame", "8");
-            }
-            break;
-        case "3":
-            if (if_right) {
-                wrap.attr("data-frame", "4");
-            } else {
-                wrap.attr("data-frame", "9");
-            }
-            break;
-        case "4":
-            if (if_right) {
-                wrap.attr("data-frame", "5");
-            } else {
-                wrap.attr("data-frame", "10");
-            }
-            break;
-        case "5":
-            if (if_right) {
-                wrap.attr("data-frame", "6");
-            } else {
-                wrap.attr("data-frame", "11");
-            }
-            break;
-        case "6":
-            if (if_right) {
-                wrap.attr("data-frame", "16");
-            } else {
-                wrap.attr("data-frame", "11");
-            }
-            break;
-        case "7":
-            if (if_right) {
-                wrap.attr("data-frame", "8");
-            } else {
-                wrap.attr("data-frame", "12");
-            }
-            break;
-        case "8":
-            if (if_right) {
-                wrap.attr("data-frame", "9");
-            } else {
-                wrap.attr("data-frame", "12");
-            }
-            break;
-        case "9":
-            if (if_right) {
-                wrap.attr("data-frame", "4");
-            } else {
-                wrap.attr("data-frame", "13");
-            }
-            break;
-        case "10":
-            if (if_right) {
-                wrap.attr("data-frame", "5");
-            } else {
-                wrap.attr("data-frame", "15");
-            }
-            break;
-        case "11":
-            if (if_right) {
-                wrap.attr("data-frame", "17");
-            } else {
-                wrap.attr("data-frame", "15");
-            }
-            break;
-        case "12":
-            if (if_right) {
-                wrap.attr("data-frame", "9");
-            } else {
-                wrap.attr("data-frame", "13");
-            }
-            break;
-        case "13":
-            if (if_right) {
-                wrap.attr("data-frame", "10");
-            } else {
-                wrap.attr("data-frame", "14");
-            }
-            break;
-        case "14":
-            if (if_right) {
-                wrap.attr("data-frame", "10");
-            } else {
-                wrap.attr("data-frame", "15");
-            }
-            break;
-        case "15":
-            if (if_right) {
-                wrap.attr("data-frame", "17");
-            } else {
-                wrap.attr("data-frame", "18");
-            }
-            break;
+    count++;
+    if (if_right) {
+        score++;
+    }
+    if (count < 19) {
+        wrap.attr("data-frame", count);
+    } else {
+        if (score > 13) {
+            wrap.attr("data-frame", 19);
+        } else if (score > 8 && score < 14) {
+            wrap.attr("data-frame", 20);
+        } else {
+            wrap.attr("data-frame", 21);
+        }
     }
 }
 function calVh() {
---
+++ todo/output/愛眼習慣線上DIY檢測/music1.as
@@ -0,0 +1,14 @@
+package
+{
+   import flash.media.Sound;
+   
+   public dynamic class music1 extends Sound
+   {
+       
+      
+      public function music1()
+      {
+         super();
+      }
+   }
+}
